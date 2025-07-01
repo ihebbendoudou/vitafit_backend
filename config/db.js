ace2b308cd3b5a93af1354bcaae3a881342fea9f
ihebbendoudou <104271032+ihebbendoudou@users.noreply.github.com>
2025-07-01 01:11:11 +0100

    Update db.js lasy

--- config/db.js
+++ config/db.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,       // pas de valeur par défaut, oblige à configurer
+    port: process.env.DB_PORT,          // Ajout du port ici
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
